677bea0c3d2f111467004c7acd474376349d9a0c
Sandeep Medikonda <103809326+sandy1in@users.noreply.github.com>
2024-11-30 09:14:49 +0530

    Time: 197 ms (75%), Space: 152.2 MB (50%) - LeetHub

--- 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.php
+++ 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.php
@@ -3,40 +3,35 @@ class Solution {
      * @param Integer[][] $pairs
      * @return Integer[][]
      */
-     
-    public function validArrangement($pairs) {
+    function validArrangement($pairs) {
         $adj = [];
         $indegree = [];
         $ans = [];
 
-        foreach ($pairs as $p) {
-            $adj[$p[0]] ??= [];
-            $adj[$p[0]][] = $p[1]; 
-            $indegree[$p[0]] = ($indegree[$p[0]] ?? 0) + 1; 
-            $indegree[$p[1]] = ($indegree[$p[1]] ?? 0) - 1; 
+        foreach ($pairs as [$u, $v]) {
+            $adj[$u][] = $v;
+            $indegree[$u] = ($indegree[$u] ?? 0) + 1;
+            $indegree[$v] = ($indegree[$v] ?? 0) - 1;
         }
 
-        $start = -1;
+        $start = array_key_first($adj);
         foreach ($indegree as $node => $degree) {
             if ($degree > 0) {
                 $start = $node;
                 break;
             }
         }
-        if ($start === -1) {
-            $start = $pairs[0][0];
-        }
 
         $this->dfs($start, $adj, $ans);
-        $ans = array_reverse($ans); 
-        return $ans;
+
+        return array_reverse($ans);
     }
 
     private function dfs($node, &$adj, &$ans) {
         while (!empty($adj[$node])) {
             $next = array_pop($adj[$node]);
-            $this->dfs($next, $adj, $ans); 
-            $ans[] = [$node, $next];
+            $this->dfs($next, $adj, $ans);
+            $ans[] = [$node, $next]; 
         }
     }
 }
